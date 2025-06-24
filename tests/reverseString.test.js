6afa7a853a9231f6963c57175dc15d61baab2f8c
AleLoiac <107403992+AleLoiac@users.noreply.github.com>
2025-06-24 16:39:14 +0200

    Add reverseString using TDD and triangulation

---
+++ tests/reverseString.test.js
@@ -0,0 +1,21 @@
+import { reverseString } from "../src/reverseString";
+
+test('Reverse "string" to equal "gnirts"', () => {
+  expect(reverseString("string")).toBe("gnirts");
+});
+
+test('Reverse "banana" to equal "ananab"', () => {
+  expect(reverseString("banana")).toBe("ananab");
+});
+
+test('Reverse "JavaScript" to equal "tpircSavaJ"', () => {
+  expect(reverseString("JavaScript")).toBe("tpircSavaJ");
+});
+
+test('Reverse "Hello World" to equal "dlroW olleH"', () => {
+  expect(reverseString("Hello World")).toBe("dlroW olleH");
+});
+
+test("Returns empty string unchanged", () => {
+  expect(reverseString("")).toBe("");
+});
